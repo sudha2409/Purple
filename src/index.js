71d3d9227d660051fa6e2dd3e286f9077b4d3253
Abhishek Bhatia <abhatia2107@gmail.com>
2023-09-06 02:51:28 +0530

    removed  basename BrowserRouter

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={"/PurpleMaze"}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
