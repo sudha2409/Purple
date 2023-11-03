e2fea37aa0dc79e8c760040e54a7d12b4b8056fe
Palak <palakchaturvedi2843@gmail.com>
2023-11-04 02:09:39 +0530

    Changes according to the document

--- src/screens/Search/ProfileDropdown.jsx
+++ src/screens/Search/ProfileDropdown.jsx
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { ROLES_LIST } from '../../api/config';
+import { Link, useNavigate } from 'react-router-dom';
+
+import './loginHeader.css';
+import useLogout from '../../hooks/useLogout';
+
 import './profileDropdown.css';
 
 const Dropdown = ({ onLogout, resetPassword, upgradeprofile }) => {
+  const navigate = useNavigate();
+  const logout = useLogout();
+  const accessAuth = JSON.parse(localStorage.getItem('accessAuth'));
+  const [freeRole, setFreeRole] = useState(true);
+  const signOut = async () => {
+    await logout();
+    navigate('/login');
+  }
+
+  const [isDropdownOpen, setIsDropdownOpen] = useState(true);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  useEffect(() => {
+  }, [isDropdownOpen]);
+
+  useEffect(() => {
+    if ((accessAuth?.roles === ROLES_LIST.Admin || accessAuth?.roles === ROLES_LIST.PaidUser)) {
+      setFreeRole(false);
+    }
+  }, [])
+
   return (
     <div className="dropdown">
       <ul className="dropdown-menu">
         <li onClick={resetPassword}>Reset Password</li>
         <li onClick={onLogout}>Logout</li>
-        <li class = "sm:hidden py-2" onClick={upgradeprofile}>Upgrade Profile</li>
+        {freeRole ?
+        <li class = "sm:hidden py-2" onClick={upgradeprofile}>Upgrade Profile</li>:""}
       </ul>
     </div>
   );
